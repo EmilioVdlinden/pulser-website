073e6d4b39ffd27e6f0ab6d50030c0aa31924b78
Emilio Van Der Linden <emilio.vanderlinden@rebin.be>
2026-02-09 19:55:03 +0100

    Update HeroSection component to use numeric values for frameBorder, marginHeight, and marginWidth

--- sections/hero-section.tsx
+++ sections/hero-section.tsx
@@ -101,9 +101,9 @@ export default function HeroSection() {
             src="https://tally.so/r/5B1zrQ?transparentBackground=1"
             width="100%"
             height="100%"
-            frameBorder="0"
-            marginHeight="0"
-            marginWidth="0"
+            frameBorder={0}
+            marginHeight={0}
+            marginWidth={0}
             title="Join waiting list"
           />
         </div>
